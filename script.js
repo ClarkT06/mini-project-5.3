f414940d8b5eb9e66b7e1ef76222581dd3e468cf
Abbey Schueller <schuellerabbey@gmail.com>
2021-03-01 21:28:15 -0800

    progress on html,css, and js

---
+++ script.js
@@ -0,0 +1,11 @@
+// var today = moment();
+// $("#date").text(today.format("MMM Do, YYYY"));
+
+var addProject = document.getElementById("addProject");
+var modal = document.getElementsByClassName("modal");
+
+
+addProject.addEventListener("click",function(event) {
+//    modal.click();
+    console.log("test");
+});
